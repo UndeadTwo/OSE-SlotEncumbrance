021c5e21451bc03a7f56b9584d1344f36e5f6bd3
Zackery Perryman <Zack@arcaderespawn.com>
2022-01-12 01:49:28 -0600

    FoundryVTT Module Template in place.

---
+++ module/init.js
@@ -0,0 +1,22 @@
+import { log } from './module.js';
+import
+
+Hooks.on('preUpdateItem', (item, changes, options, ...args) => {
+
+});
+
+Hooks.on('createItem', (item, options, userId, ...args) => {
+
+});
+
+Hooks.on('deleteItem', (item, options, userId, ...args) => {
+
+});
+
+Hooks.on('updateItem', (item, changes, options, userId) => {
+
+});
+
+Hooks.on('renderActorSheet', (actorSheet) => {
+
+});
