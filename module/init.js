c3d54dfb92bfde80e9cf86ade680255988b604d4
Zackery Perryman <Zack@arcaderespawn.com>
2022-04-21 21:55:25 -0500

    Testing update path to continue learning foundry.

--- module/init.js
+++ module/init.js
@@ -1,22 +1,21 @@
 import { log } from './module.js';
-import
 
 Hooks.on('preUpdateItem', (item, changes, options, ...args) => {
-
+	log.debug('Pre-Update Item', item, changes, options)
 });
 
 Hooks.on('createItem', (item, options, userId, ...args) => {
-
+	log.debug('Create Item', item, options, userId)
 });
 
 Hooks.on('deleteItem', (item, options, userId, ...args) => {
-
+	log.debug('Delete Item', item, options, userId)
 });
 
 Hooks.on('updateItem', (item, changes, options, userId) => {
-
+	log.debug('Update Item', item, changes, options, userId)
 });
 
 Hooks.on('renderActorSheet', (actorSheet) => {
-
+	log.debug('Render Actor Sheet', actorSheet)
 });
